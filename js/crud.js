d9abf3eb5b66a61bc4ba32ab0e39b3c17092a354
Brad Hick <bradleyhrm@gmail.com>
2017-02-06 02:02:40 -0200

    Function Adicionar produto e Reseta Form
    Inclusão a opçã que adiciona produtos e uama pra resetar o form sempre
    que necessário

--- js/crud.js
+++ js/crud.js
@@ -6,8 +6,40 @@ var removerItem = function(event){
 }
 
 
+var resetaForm = function(){
+  $("#nome").val("");
+  $("#preco").val("");
+  $("#descricao").val("");
+
+}
+
+
+var adicionarItem = function(event){
+  event.preventDefault();
+  var nome = $("#nome").val();
+  var preco = parseFloat($("#preco").val());
+  var descricao = $("#descricao").val();
+
+
+  var tr = $("<tr>").append($("<td>").text(nome).addClass("nome-prod"));
+  tr.append($("<td>").addClass("preco-prod").text(preco));
+  tr.append($("<td>").addClass("descricao-prod").text(descricao));
+
+  var tdAcoes = $("<td>").append($("<a>").text("Editar").addClass("editar-item").attr("href", ""));
+  tdAcoes.append("  |  ");
+  tdAcoes.append($("<a>").text("Excluir").addClass("excluir-item").attr("href", ""));
+
+  tr.append(tdAcoes);
+
+  $(".produtos").find("tbody").append(tr);
+  $(".excluir-item").click(removerItem);
+  resetaForm();
+
+}
+
 var aposInicializado = function(){
   $(".excluir-item").click(removerItem);
+  $(".botao-home").click(adicionarItem);
 
 
 }
